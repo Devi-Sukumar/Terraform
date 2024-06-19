ec58563c096b904619078bd0fbafd3ab991a9611
Devi-Sukumar <117468528+Devi-Sukumar@users.noreply.github.com>
2024-06-19 16:41:16 +0530

    Updated assignment3

--- provider.tf
+++ provider.tf
@@ -1,3 +1,4 @@
+
 # provider "aws" {
 #        region = "us-east-2"
 #        access_key = ""     ## Add ypur key
@@ -9,4 +10,12 @@ provider "aws" {
   shared_config_files      = [".aws/config"]
   shared_credentials_files = [".aws/credentials"]
   profile                  = "mytfprofile"
+}
+
+## N.Virginia : us-east-1 -- Added for assignment3
+provider "aws" {
+  alias = "east1"
+  shared_config_files      = [".aws/config"]
+  shared_credentials_files = [".aws/credentials"]
+  profile                  = "hellonv"
 }
