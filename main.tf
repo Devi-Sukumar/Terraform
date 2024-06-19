80d8046f2c18152080a9a8df4cbdabb89f9cd1d3
Devi-Sukumar <117468528+Devi-Sukumar@users.noreply.github.com>
2024-06-19 16:42:37 +0530

    Updated assignment3

--- main.tf
+++ main.tf
@@ -8,22 +8,43 @@
 #         }
 # }
 
-## Second instance
-resource "aws_instance" "assign2" {
+## Second instance -- commented to destroy the previous deployment from assignment2
+# resource "aws_instance" "assign2" {
+#         ami = "ami-09040d770ffe2224f"
+#         instance_type = "t2.micro"
+#         key_name = "tfkey"
+#         tags = {
+#         Name = "Tf-assignment-2"
+#         }
+# }
+
+# ## Elastic IP for second instance -- commented to destroy the previous deployment from assignment2
+# resource "aws_eip" "eip" {
+#   vpc = true
+# }
+
+# resource "aws_eip_association" "eip_assoc" {
+#   instance_id   = aws_instance.assign2.id
+#   allocation_id = aws_eip.eip.id
+# }
+
+## Third instance 'hello-ohio'
+resource "aws_instance" "ohioassign3" {
         ami = "ami-09040d770ffe2224f"
         instance_type = "t2.micro"
         key_name = "tfkey"
         tags = {
-        Name = "Tf-assignment-2"
+        Name = "hello-ohio"
         }
 }
 
-## Elastic IP for second instance
-resource "aws_eip" "eip" {
-  vpc = true
-}
-
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.assign2.id
-  allocation_id = aws_eip.eip.id
+## Fourth instance 'hello-virginia'
+resource "aws_instance" "nvassign3" {
+        provider = aws.east1
+        ami = "ami-04b70fa74e45c3917"
+        instance_type = "t2.micro"
+        key_name = "casestudy"
+        tags = {
+        Name = "hello-virginia"
+        }
 }
