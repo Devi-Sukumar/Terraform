36c84e36ddef139f10e7c3038d8eeb6d27e3bf57
Devi-Sukumar <117468528+Devi-Sukumar@users.noreply.github.com>
2024-06-19 16:39:38 +0530

    Updated assignment2

--- main.tf
+++ main.tf
@@ -1,8 +1,29 @@
-resource "aws_instance" "assign1" {
+## First instance -- commented to destroy the previous deployment from assignment1
+# resource "aws_instance" "assign1" {
+#         ami = "ami-09040d770ffe2224f"
+#         instance_type = "t2.micro"
+#         key_name = "tfkey"
+#         tags = {
+#         Name = "Terraform assignment-1"
+#         }
+# }
+
+## Second instance
+resource "aws_instance" "assign2" {
         ami = "ami-09040d770ffe2224f"
         instance_type = "t2.micro"
         key_name = "tfkey"
         tags = {
-        Name = "Terraform assignment-1"
+        Name = "Tf-assignment-2"
         }
+}
+
+## Elastic IP for second instance
+resource "aws_eip" "eip" {
+  vpc = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.assign2.id
+  allocation_id = aws_eip.eip.id
 }
