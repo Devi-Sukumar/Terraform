b52e86ac2368239c5230046dbc39e50404eb6270
Devi-Sukumar <117468528+Devi-Sukumar@users.noreply.github.com>
2024-06-19 16:43:53 +0530

    Updated Assignment 4

--- main.tf
+++ main.tf
@@ -28,23 +28,50 @@
 #   allocation_id = aws_eip.eip.id
 # }
 
-## Third instance 'hello-ohio'
-resource "aws_instance" "ohioassign3" {
-        ami = "ami-09040d770ffe2224f"
+# ## Third instance 'hello-ohio' -- commented to destroy the previous deployment from assignment3
+# resource "aws_instance" "ohioassign3" {
+#         ami = "ami-09040d770ffe2224f"
+#         instance_type = "t2.micro"
+#         key_name = "tfkey"
+#         tags = {
+#         Name = "hello-ohio"
+#         }
+# }
+
+# ## Fourth instance 'hello-virginia' -- commented to destroy the previous deployment from assignment3
+# resource "aws_instance" "nvassign3" {
+#         provider = aws.east1
+#         ami = "ami-04b70fa74e45c3917"
+#         instance_type = "t2.micro"
+#         key_name = "casestudy"
+#         tags = {
+#         Name = "hello-virginia"
+#         }
+# }
+
+## Fifth instance - Assignment 4
+resource "aws_instance" "assign-4" {
+        ami = "ami-09040d770ffe2224f" 
         instance_type = "t2.micro"
-        key_name = "tfkey"
+        subnet_id = aws_subnet.assign-4-subnet.id
+        key_name = "tfkey"          
         tags = {
-        Name = "hello-ohio"
+        Name = "Tf-assign-4"
         }
 }
 
-## Fourth instance 'hello-virginia'
-resource "aws_instance" "nvassign3" {
-        provider = aws.east1
-        ami = "ami-04b70fa74e45c3917"
-        instance_type = "t2.micro"
-        key_name = "casestudy"
+resource "aws_vpc" "assign-4-vpc" {
+        cidr_block = "10.0.0.0/16"
         tags = {
-        Name = "hello-virginia"
+        Name = "assignment-4-vpc"
+        }
+}
+
+resource "aws_subnet" "assign-4-subnet" {
+        vpc_id = aws_vpc.assign-4-vpc.id
+        cidr_block = "10.0.1.0/24"
+        availability_zone = "us-east-2a"
+        tags = {
+        Name = "assign-4-subnet"
         }
 }
